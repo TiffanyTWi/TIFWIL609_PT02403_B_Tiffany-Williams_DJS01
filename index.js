f830a53c1ab9ba5571f379054bfe7edfd1f85ad2
Tiffany Williams <tiffanywilliams870@gmail.com>
2024-10-19 22:13:48 +0200

    DJS01

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
-
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const velocityKmh = 10000; // velocity (km/h)
+const accelerationMs2 = 3; // acceleration (m/s^2)
+const timeInSeconds = 3600; // seconds (1 hour)
+const startingDistanceKm = 0; // distance (km)
+const startingFuelKg = 5000; // remaining fuel (kg)
+const fuelBurnRateKgs = 0.5; // fuel burn rate (kg/s)
 
+// Calculates new distance
+// Convert velocity from km/h to m/s
+const velocityMs = (10000/3600);
+const newDistance = startingDistanceKm + (velocityMs * timeInSeconds)
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+// Calculates remaining fuel
+const remainingFuel = startingFuelKg - (fuelBurnRateKgs * timeInSeconds)
 
-// Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+// Calculates new velocity
+const calculateNewVelocity = (velocityKmh, accelerationMs2, timeInSeconds)
+const newVelocityKmh = calculateNewVelocity + velocityKmh + (accelerationMs2 * timeInSeconds);
+
+try {
+console.log(`Corrected New Velocity: ${newVelocityKmh} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
+
+} catch (error) {
+  console.error(`Error: ${error.message}`);
 }
-
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
-
-
